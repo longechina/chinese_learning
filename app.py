4fcc65e972607a6ede12f61f49e301ccaec8c0bb
Longe/Longmin <2220524364@qq.com>
2026-03-24 03:28:01 +0800

    update

--- app.py
+++ app.py
@@ -486,7 +486,7 @@ st.markdown(f"""
     }}
 
     .stApp {{
-        background-color: rgba(255, 255, 255, 0.6) !important;
+        background-color: rgba(255, 255, 255, 0.5) !important;
         background-blend-mode: overlay !important;
     }}
 
